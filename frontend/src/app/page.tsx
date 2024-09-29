c763ca6b5596d5eb5b05a8af9280aba0611ec404
Natthawat Narin <natthawat.narin@gmail.com>
2024-09-29 20:17:28 +0700

    add test shadcn ui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,37 +1,54 @@
-import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <div className="flex gap-2">
+        <Button>Click me</Button>
+        <Button variant={"destructive"}>Click me</Button>
+        <Button variant={"secondary"}>Click me</Button>
+        <Button variant={"outline"}>Click me</Button>
       </div>
-    </main>
+
+      <div className="flex gap-2 m-10">
+        Chuck Norris’ tears cure cancer. Too bad he has never cried. Chuck
+        Norris can sit in the corner of a round room. Chuck Norris is the reason
+        why Waldo is hiding. Chuck Norris once ate at Taco Bell and didn’t get
+        diarrhea. Chuck Norris named his daughter Mercy. The day she was born
+        was the only day Chuck Norris ever had Mercy. Chuck Norris doesn’t wear
+        a watch. He decides what time it is. Freddy Krueger has nightmares about
+        Chuck Norris. Chuck Norris once roundhouse kicked someone so hard that
+        his foot broke the speed of light. When Chuck Norris gives you the
+        finger, he’s telling you how many seconds you have left to live. Chuck
+        Norris can kill your imaginary friends. Chuck Norris has never blinked
+        in his entire life. Never. Chuck Norris does not own a stove, oven, or
+        microwave , because revenge is a dish best served cold. Chuck refers to
+        himself in the fourth person. It takes Chuck Norris 20 minutes to watch
+        60 Minutes. Chuck Norris’ cowboy boots are made from real cowboys. Chuck
+        Norris once roundhouse kicked someone so hard that his foot broke the
+        speed of light. Chuck Norris once ate at Taco Bell and didn’t get
+        diarrhea. Chuck Norris does not use spell check. If he happens to
+        misspell a word, Oxford will change the spelling. Chuck Norris sleeps
+        with a pillow under his gun. Chuck Norris once ordered a steak in a
+        restaurant. The steak did what it was told. When Chuck Norris writes, he
+        makes paper bleed. Bigfoot claims he once saw Chuck Norris. The flu gets
+        a Chuck Norris shot every year. Chuck Norris once ate at Taco Bell and
+        didn’t get diarrhea. Chuck Norris can play the violin with a piano.
+        Chuck Norris sleeps with a pillow under his gun. Chuck Norris plays
+        Jenga with Stonehenge. Chuck Norris can hear sign language. Chuck Norris
+        can clap with one hand. On New Year’s Eve, Chuck Norris promised that
+        he’d lose 20 pounds. The next morning he shaved his chest and smiled as
+        he realized that he’d lost 30. Chuck Norris can hear sign language.
+        Chuck Norris can make a slinky go upstairs. Chuck Norris has a mug of
+        nails instead of coffee in the morning. It takes Chuck Norris 20 minutes
+        to watch 60 Minutes. Chuck Norris lost his virginity before his dad did.
+        Chuck Norris once shattered the space-time continuum. He felt so bad, he
+        put it back together. When God said, “Let there be light!” Chuck said,
+        “Say Please.”. Chuck Norris’ tears cure cancer. Too bad he has never
+        cried. Chuck Norris can have both feet on the ground and kick butt at
+        the same time. Chuck Norris doesn’t cheat death. He wins fair and
+        square.
+      </div>
+    </div>
   );
 }
