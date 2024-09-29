046e06054dbce0cb52de4bbb44991d6bf65a56d2
Natthawat Narin <natthawat.narin@gmail.com>
2024-09-29 20:48:09 +0700

    add backend Prisma Module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { configValidationSchema } from 'config/config.validation';
+import { PrismaModule } from 'lib/prisma/prisma.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { configValidationSchema } from 'config/config.validation';
       isGlobal: true,
       validationSchema: configValidationSchema,
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
