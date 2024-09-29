195262eb4d01f478d0f3e7047f0f1be8c396a790
Natthawat Narin <natthawat.narin@gmail.com>
2024-09-30 01:58:11 +0700

    add feature session

---
+++ frontend/src/lib/session.ts
@@ -0,0 +1,61 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { decodeJwt, type UserJwtPayload } from "@/lib/user-jwt";
+
+export async function createSession(data: {
+  accessToken: string;
+  user: UserJwtPayload;
+}) {
+  // Set the session expiration time
+  const expiresAt = new Date(data.user.exp * 1000);
+  cookies().set("session", data.accessToken, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: "lax",
+    path: "/",
+  });
+}
+
+export async function getSessionAPIAccessToken() {
+  const accessToken = cookies().get("session")?.value;
+  return { accessToken };
+}
+
+export async function getSessionUser() {
+  const accessToken = cookies().get("session")?.value;
+  if (!accessToken) return { user: undefined };
+  const { data: user, error } = decodeJwt(accessToken);
+  if (error ?? !user) {
+    return { user: undefined };
+  }
+  return {
+    user,
+  };
+}
+
+export async function verifySession() {
+  const accessToken = cookies().get("session")?.value;
+
+  if (!accessToken) {
+    redirect("/auth/sign-in");
+  }
+
+  const { data: user, error } = decodeJwt(accessToken);
+  if (error ?? !user) {
+    redirect("/auth/sign-in");
+  }
+
+  return {
+    isLoggedIn: true,
+    user,
+    accessToken,
+  };
+}
+
+export async function deleteSession() {
+  cookies().delete("session");
+  redirect("/auth/sign-in");
+}
