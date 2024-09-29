b3336df4e64d1747df981f649aefc0ff10617755
Natthawat Narin <natthawat.narin@gmail.com>
2024-09-30 01:53:39 +0700

    add auth use-cases

--- frontend/src/use-cases/auth/auth.use-case.ts
+++ frontend/src/use-cases/auth/auth.use-case.ts
@@ -2,7 +2,7 @@
 import { type APIErrorResponse } from "@/interfaces/services/base";
 import { type LoginDto, type LoginResponse } from "@/interfaces/services/auth";
 import { type login } from "@/interfaces/use-cases/auth.use-case.d";
-import { handleAPIError } from "../base/base.use-case";
+import { decodeJwt } from "lib/user-jwt";
 
 export async function loginUseCase(params: {
   context: {
@@ -13,5 +13,48 @@ export async function loginUseCase(params: {
   const { context, data } = params;
 
   const resp = await context.login(data);
-  return handleAPIError(resp);
+
+  if (resp.error ?? !resp.data) {
+    const apiError = resp.error?.response?.data as APIErrorResponse;
+    const statusCode = resp.error?.response?.status ?? 500;
+    const errorText = resp.error?.response?.statusText;
+
+    if (apiError?.statusCode == 401) {
+      return {
+        error: {
+          statusCode,
+          error: errorText,
+          message: "Invalid email or password.",
+        },
+      };
+    }
+
+    return {
+      error: {
+        statusCode,
+        error: errorText,
+        message:
+          apiError?.message ??
+          resp.error?.response?.statusText ??
+          resp.error?.message ??
+          "An unexpected error occurred. Please try again.",
+      },
+    };
+  }
+
+  const userJwt = decodeJwt(resp.data.accessToken);
+
+  if (userJwt.error ?? !userJwt.data) {
+    return {
+      error: {
+        statusCode: 500,
+        error: userJwt.error?.message,
+        message:
+          userJwt.error?.message ??
+          "An unexpected error occurred. Please try again.",
+      },
+    };
+  }
+
+  return { data: resp.data };
 }
