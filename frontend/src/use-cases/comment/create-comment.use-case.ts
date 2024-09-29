d62c48f5681bdb1558e6b100ae5d2235ba124e50
Natthawat Narin <natthawat.narin@gmail.com>
2024-09-30 01:40:13 +0700

    add create comment use-cases

---
+++ frontend/src/use-cases/comment/create-comment.use-case.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { type APIErrorResponse } from "@/interfaces/services/base";
+import {
+  type Comment as CommentModel,
+  type CreateCommentDto,
+} from "@/interfaces/services/comment";
+import { type createComment } from "@/interfaces/use-cases/comment.use-case.d";
+import { handleAPIError } from "../base/base.use-case";
+import { type getProfile } from "@/interfaces/use-cases/base.use-case";
+
+export async function createCommentUseCase(params: {
+  context: {
+    getProfile: getProfile;
+    createComment: createComment;
+  };
+  data: CreateCommentDto;
+}): Promise<{ data?: CommentModel; error?: APIErrorResponse }> {
+  const { context, data } = params;
+
+  const user = await context.getProfile();
+  if (!user) {
+    return {
+      error: {
+        statusCode: 401,
+        error: "Unauthorized",
+        message: "User not authenticated. Please try again.",
+      },
+    };
+  }
+
+  const resp = await context.createComment(data);
+  return handleAPIError(resp);
+}
