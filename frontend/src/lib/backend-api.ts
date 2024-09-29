a089fb02e4f597982d7e3859ab88dafeedb7f85c
Natthawat Narin <natthawat.narin@gmail.com>
2024-09-30 02:17:13 +0700

    add lib backend api

---
+++ frontend/src/lib/backend-api.ts
@@ -0,0 +1,36 @@
+import axios, { type AxiosError, type AxiosResponse } from "axios";
+import { getSessionAPIAccessToken } from "./session";
+import { env } from "@/env";
+
+const backendApi = axios.create({
+  baseURL: env.BACKEND_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 5000,
+});
+
+// Add a request interceptor to include the Authorization header
+backendApi.interceptors.request.use(
+  async (config) => {
+    const session = await getSessionAPIAccessToken();
+
+    if (session?.accessToken) {
+      config.headers.Authorization = `Bearer ${session.accessToken}`;
+    }
+
+    return config;
+  },
+  (error: AxiosError) => Promise.reject(error),
+);
+
+// Add a response interceptor for handling errors
+backendApi.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  (error: AxiosError) => {
+    // Optional: Handle specific response errors globally
+    return Promise.reject(error);
+  },
+);
+
+export default backendApi;
