f4ea7bab616b1e3688024dcafd929ebbe18f78fd
Natthawat Narin <natthawat.narin@gmail.com>
2024-09-29 20:41:21 +0700

    add backend feature jwt

---
+++ backend/interface/request.interface.ts
@@ -0,0 +1,10 @@
+export interface JwtPayload {
+  id: number;
+  username: string;
+}
+
+// Extend the Request interface to include the user property.
+// This is used when applying @UseGuards(AuthGuard('jwt')) on a controller.
+export interface RequestWithUser extends Request {
+  user: JwtPayload;
+}
