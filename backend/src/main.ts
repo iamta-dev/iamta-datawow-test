649202ef2205b191b0970c9262b1f94de8c7ce4f
Natthawat Narin <natthawat.narin@gmail.com>
2024-09-29 20:34:04 +0700

    add PORT and CORS_ALLOWED_ORIGINS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS.split(',');
+
+  const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
+
+  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log('Allowed origins:', allowedOrigins);
 }
 bootstrap();
