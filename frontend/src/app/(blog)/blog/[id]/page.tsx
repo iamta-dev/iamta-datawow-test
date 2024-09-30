e159f97ee20ff9fd0a72150a0ca9b0f258052153
Natthawat Narin <natthawat.narin@gmail.com>
2024-09-30 21:25:28 +0700

    sync api on blogId page

--- frontend/src/app/(blog)/blog/[id]/page.tsx
+++ frontend/src/app/(blog)/blog/[id]/page.tsx
@@ -3,13 +3,39 @@
 import { useSidebarState } from "@/hooks/use-sidebar";
 import { ArrowLeft, MessageCircle } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState, useTransition } from "react";
 import { CreateCommentFormDialog } from "./_components/create-comment-form-dialog";
 import { CreateCommentForm } from "./_components/create-comment-form";
 import { Button } from "@/components/ui/button";
+import { type Post as PostModel } from "@/interfaces/services/post";
+import {
+  getPostByIdAction,
+  PostResultState,
+} from "../../_actions/get-post.action";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+
+import { toast } from "sonner";
+import { formatTimeAgo } from "@/lib/date-format";
 
 export default function BlogIdPage({ params }: { params: { id: string } }) {
   const router = useRouter();
+
+  const [isPending, startTransition] = useTransition();
+
+  const [post, setPost] = useState<PostModel | undefined>(undefined);
+
+  const fetchData = async (id: number): Promise<PostResultState> => {
+    const resp = await getPostByIdAction(id);
+    if (resp?.result) {
+      setPost(resp.result);
+    }
+
+    if (resp?.status == "error") {
+      toast.error(resp.message);
+    }
+    return resp;
+  };
 
   const { isSidebarOpen, toggleSidebar } = useSidebarState(); // Get the sidebar state (true/false)
 
@@ -19,16 +45,6 @@ export default function BlogIdPage({ params }: { params: { id: string } }) {
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
-  };
-
-  const post = {
-    id: 1,
-    author: "Wittawat",
-    category: "History",
-    title: "The Beginning of the End of the World",
-    description:
-      "The afterlife sitcom The Good Place comes to its culmination...",
-    comments: 32,
   };
 
   // Function to toggle the form for desktop
@@ -48,6 +64,15 @@ export default function BlogIdPage({ params }: { params: { id: string } }) {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
+  useEffect(() => {
+    startTransition(async () => {
+      const postId = Number(params.id);
+      if (!isNaN(postId)) {
+        await fetchData(postId);
+      }
+    });
+  }, []);
 
   return (
     <div>
@@ -74,30 +99,32 @@ export default function BlogIdPage({ params }: { params: { id: string } }) {
         }`}
       >
         {/* Post Section */}
-        <article key={post.id} className="rounded-md bg-white p-6 shadow-md">
+        <article key={post?.id} className="rounded-md bg-white p-6 shadow-md">
           {/* Author Info */}
-          <div className="mb-4 flex items-center">
-            <div className="mr-4">
-              {/* Avatar */}
-              <div className="h-12 w-12 rounded-full bg-gray-300"></div>
-            </div>
-            <div>
-              <div className="flex items-center">
-                <span className="text-lg font-bold">{post.author}</span>
-                <span className="ml-2 text-sm text-gray-500">5mo. ago</span>
+          <div className="flex flex-col items-start gap-2">
+            <div className="flex items-center justify-start gap-2">
+              <Avatar>
+                <AvatarImage src={post?.user?.pictureUrl} alt="Post Profile" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div className="font-semibold text-black">
+                {post?.user?.fullName}
               </div>
-              <div className="text-sm text-gray-500">{post.category}</div>
+              <div className="text-sm text-gray-500">
+                {post?.createdAt ? formatTimeAgo(post.createdAt) : ""}
+              </div>
             </div>
+            <Badge variant={"grey"}>{post?.community?.name}</Badge>
           </div>
 
           {/* Post Content */}
-          <h2 className="text-2xl font-bold">{post.title}</h2>
-          <p className="mt-4 text-gray-700">{post.description}</p>
+          <h2 className="text-2xl font-bold">{post?.title}</h2>
+          <p className="mt-4 text-gray-700">{post?.detail}</p>
 
           {/* Total Comments Section */}
           <div className="mt-6 flex items-center text-gray-500">
             <MessageCircle className="mr-2 h-5 w-5" />
-            <div>{post.comments} Comments</div>
+            <div>{post?.comments?.length ?? 0} Comments</div>
           </div>
 
           {/* Add Comments Button (Only show if form is not visible on Desktop) */}
@@ -124,59 +151,33 @@ export default function BlogIdPage({ params }: { params: { id: string } }) {
 
           {/* Desktop Comment Form */}
           {isCommentFormVisible && (
-            // <div className="mt-6">
-            //   <textarea
-            //     className="mb-4 w-full rounded border border-gray-300 p-2"
-            //     placeholder="Write your comment..."
-            //   />
-            //   <div className="flex space-x-4">
-            //     <button
-            //       className="rounded bg-red-500 px-4 py-2 text-white hover:bg-red-600"
-            //       onClick={hideCommentForm}
-            //     >
-            //       Cancel
-            //     </button>
-            //     <button className="rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600">
-            //       Post
-            //     </button>
-            //   </div>
-            // </div>
-
             <CreateCommentForm hideCommentForm={hideCommentForm} />
           )}
 
           {/* Comments */}
           <div className="mt-8 p-6">
-            {[
-              {
-                author: "Wittawat98",
-                timeAgo: "12h ago",
-                content: "Lorem ipsum dolor sit amet consectetur.",
-              },
-              {
-                author: "Hawaii51",
-                timeAgo: "1mo. ago",
-                content: "Lorem ipsum dolor sit amet consectetur.",
-              },
-              {
-                author: "Helo_re",
-                timeAgo: "3mo. ago",
-                content: "Lorem ipsum dolor sit amet consectetur.",
-              },
-            ].map((comment, idx) => (
+            {post?.comments?.map((comment, idx) => (
               <div key={idx} className="mb-4 flex items-start">
                 <div className="mr-4">
                   {/* Avatar */}
-                  <div className="h-10 w-10 rounded-full bg-gray-300"></div>
+                  <Avatar>
+                    <AvatarImage
+                      src={comment?.user?.pictureUrl}
+                      alt="Comment Profile"
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
                 </div>
                 <div>
                   <div className="flex items-center">
-                    <span className="font-bold">{comment.author}</span>
+                    <span className="font-bold">{comment.user?.fullName}</span>
                     <span className="ml-2 text-sm text-gray-500">
-                      {comment.timeAgo}
+                      {comment?.createdAt
+                        ? formatTimeAgo(comment.createdAt)
+                        : ""}
                     </span>
                   </div>
-                  <p className="text-gray-700">{comment.content}</p>
+                  <p className="text-gray-700">{comment.comment}</p>
                 </div>
               </div>
             ))}
