ce4e95a1a0d07afbb4b6dc4577cfd9703c6acdbc
Natthawat Narin <natthawat.narin@gmail.com>
2024-09-30 16:18:36 +0700

    change ux sideBar

--- frontend/src/components/blog/useSidebarState.tsx
+++ frontend/src/components/blog/useSidebarState.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import type { PropsWithChildren } from "react";
 
 // Define the type for the sidebar context
@@ -14,12 +14,36 @@ const SidebarContext = createContext<SidebarContextProps | null>(null);
 
 // Create a provider component with children as props
 export function SidebarProvider({ children }: PropsWithChildren) {
-  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(true); // Default to sidebar open
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false); // Default to sidebar open
 
   // Function to toggle sidebar visibility
   const toggleSidebar = () => {
     setIsSidebarOpen((prevState) => !prevState);
   };
+
+  useEffect(() => {
+    // Check screen size on mount and set the sidebar state accordingly
+    const handleResize = () => {
+      if (window.innerWidth < 900) {
+        // For tablet and mobile screens default to sidebar closed
+        setIsSidebarOpen(false);
+      } else {
+        // For non-tablet screens, default to sidebar open
+        setIsSidebarOpen(true);
+      }
+    };
+
+    // Set sidebar state based on the initial window width
+    handleResize();
+
+    // Add a resize event listener to update sidebar state when window is resized
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener when component is unmounted
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <SidebarContext.Provider
