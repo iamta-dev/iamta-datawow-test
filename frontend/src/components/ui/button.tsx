3a1b437cbc57910723cf4788d30d7c66b74adf39
Natthawat Narin <natthawat.narin@gmail.com>
2024-09-30 13:18:07 +0700

    change custom button component

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -13,10 +13,9 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-outline-foreground bg-outline text-outline-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+          "border border-secondary-foreground bg-secondary text-secondary-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
