b90f9f0852c8ba9ac03182b2bb774e6e9ae5feb2
Natthawat Narin <natthawat.narin@gmail.com>
2024-09-30 09:09:02 +0700

    disable middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -4,9 +4,9 @@ export function middleware(req: NextRequest) {
   const sessionCookie = req.cookies.get("session"); // Adjust the cookie name if necessary
 
   // If the session cookie is missing, redirect to the sign-in page
-  if (!sessionCookie) {
-    return NextResponse.redirect(new URL("/auth/sign-in", req.url));
-  }
+  // if (!sessionCookie) {
+  //   return NextResponse.redirect(new URL("/auth/sign-in", req.url));
+  // }
 
   // If session cookie exists, proceed with the request
   return NextResponse.next();
