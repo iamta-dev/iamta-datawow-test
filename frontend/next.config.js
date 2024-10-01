0fbaf16a3607710d0f3148b0875e7794237b14cc
Natthawat Narin <natthawat.narin@gmail.com>
2024-10-02 02:54:50 +0700

    add feature deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,13 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    output: "standalone",
+    reactStrictMode: true,
+    swcMinify: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
 
 export default config;
