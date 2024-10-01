f544e93dddb0a9c3cc6d6ace60c3e8f8b9ca5e60
Natthawat Narin <natthawat.narin@gmail.com>
2024-10-01 22:48:00 +0700

    fix themes login page

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,15 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import { useFormState, useFormStatus } from "react-dom";
+import { useFormState } from "react-dom";
 import { loginAction } from "../../../actions/auth.action";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function LoginPage() {
   const [state, action] = useFormState(loginAction, undefined);
 
   return (
-    <div className="flex h-screen flex-col bg-green-900 md:flex-row">
+    <div className="flex h-screen flex-col bg-green-500 md:flex-row">
       {/* Right Side: a Board (Desktop) / Top (Mobile) */}
-      <div className="order-first flex h-2/5 w-full items-center justify-center rounded-b-[2rem] bg-green-700 md:order-last md:h-auto md:w-2/5 md:rounded-l-[2rem]">
+      <div className="order-first flex h-2/5 w-full items-center justify-center rounded-b-[2rem] bg-green-300 md:order-last md:h-auto md:w-2/5 md:rounded-l-[2rem]">
         <div className="flex flex-col items-center">
           <img
             src="/images/board.png"
@@ -21,27 +23,25 @@ export default function LoginPage() {
       </div>
 
       {/* Left Side: Sign-in (Desktop) / Bottom (Mobile) */}
-      <div className="order-last flex h-3/5 w-full items-center justify-center bg-green-900 md:order-first md:h-auto md:w-3/5">
-        <div className="w-3/4 max-w-md">
-          <h1 className="mb-6 text-3xl text-white">Sign in</h1>
+      <div className="order-last flex h-3/5 w-full items-center justify-center bg-green-500 md:order-first md:h-auto md:w-3/5">
+        <div className="w-11/12 sm:w-4/5 max-w-sm flex flex-col justify-center">
+          <h1 className="mb-6 text-3xl font-semibold text-white">Sign in</h1>
           <form className="space-y-4" action={action}>
-            <input
+            <Input
               type="text"
               placeholder="Username"
               name="username"
-              className="w-full rounded-md p-3 text-black"
+              className="w-full"
             />
             {state?.errors?.username && (
-              <p className="mb-2 text-sm text-red-500">
+              <p className="mb-2 text-sm text-destructive">
                 {state.errors.username}
               </p>
             )}
             {state?.message && (
-              <p className="mt-5 text-sm text-red-500">{state.message}</p>
+              <p className="mt-5 text-sm text-destructive">{state.message}</p>
             )}
-            <button className="w-full rounded-md bg-green-500 p-3 text-white">
-              Sign In
-            </button>
+            <Button className="w-full font-semibold">Sign In</Button>
           </form>
         </div>
       </div>
